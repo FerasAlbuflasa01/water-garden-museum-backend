ace0eb48809d9e837f80b68691fa5af47722379c
FerasGIT <ferasny@gmail.com>
2025-08-05 21:55:21 +0300

    new mergre

--- routes/GameRouter.js
+++ routes/GameRouter.js
@@ -1,9 +1,10 @@
 const router = require('express').Router()
 const middleware = require('../middleware')
 const controller = require('../Controllers/GameController')
+const { checkSession } = require('../Controllers/authConrtollers')
 
 router.post('/new', middleware.verifyToken, controller.newGamess)
 
-router.get('/session', middleware.verifyToken, controller.checkSession)
+router.get('/session', middleware.verifyToken, checkSession)
 
 module.exports = router
