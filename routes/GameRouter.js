651580c954c547f492eb04914faeaf9580ec9cd2
alqaassass <ali.alqassas02@gmail.com>
2025-08-05 17:58:00 +0300

    game card all good

--- routes/GameRouter.js
+++ routes/GameRouter.js
@@ -3,6 +3,8 @@ const middleware = require('../middleware')
 const controller = require('../Controllers/GameController')
 
 router.post('/new', middleware.verifyToken, controller.newGamess )
+router.get('/' , controller.showGames)
+router.get('/:gameId' , controller.showGamesDetails)
 
 // router.get('/session', middleware.verifyToken, controller.checkSession)
 
